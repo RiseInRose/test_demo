88b3ac0bdb607623c521b55655976f9a5e87b4ed
caturbhujadas <475417235@qq.com>
2019-07-30 11:14:26 +0800

    添加mongo 同步异步demo

--- thread_demo/测试线程抢占CPU.py
+++ thread_demo/测试线程抢占CPU.py
@@ -7,5 +7,33 @@
 但是现在想来，可能出现，queue一直被写线程抢占，导致读线程没有机会执行。
 现在来设计案例测试下。
 """
+from threading import Thread
+from queue import Queue
+import time
+q = Queue(10)
 
 
+def put():
+    cnt = 0
+    while 1:
+        # cnt += 1
+        # q.put(cnt)
+        # print("put cnt:{}".format(cnt))
+
+        q.put(1)
+        print("put cnt")
+
+
+def get():
+    while 1:
+        print("get cnt:{}".format(q.get()))
+        # time.sleep(0.001)
+
+
+if __name__ == '__main__':
+    Thread(target=put).start()
+    Thread(target=get).start()
+
+'''
+观察结果，在mac的cpu会有时间片的概念。每个子线程会分配到相同的时间片。
+'''
